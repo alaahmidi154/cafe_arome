bff29581f3aa8d2c1c3375829ca4cf43ed801414
Alaa <elhmidialaa@gmail.com>
2026-06-13 21:13:39 +0100

    Migrate from CSS to SCSS

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.css']
+  styleUrls: ['./reservation.component.scss']
 })
 export class ReservationComponent {
   submitted = false;
